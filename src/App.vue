6eae3de033b6703e58a7b4099eea3a088188e799
Sebastian Umiński <uminski93@gmail.com>
2020-04-20 18:56:26 +0200

    Import fonts

--- src/App.vue
+++ src/App.vue
@@ -10,4 +10,20 @@ export default {
 </script>
 
 <style lang="scss">
+  @import './assets/fonts/fonts.css';
+
+  * {
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  body {
+    font-size: 18px;
+    font-family: Muli, sans-serif;
+    font-weight: 400;
+    margin: 0;
+    padding: 0;
+  }
+
 </style>
